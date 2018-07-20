58b5db3855993efe89aa3f746b1a662bffde7544
lhxcy <lh_xcy@163.com>
2018-07-20 10:21:06 +0800

    7.20 更改数据产生格式

--- src/main/java/com/tech/analysis/Dao/GetEnterpriseAndExpertData.java
+++ src/main/java/com/tech/analysis/Dao/GetEnterpriseAndExpertData.java
@@ -1,7 +1,6 @@
 package com.tech.analysis.Dao;
 
 import com.tech.analysis.entity.AuthorEntity;
-import org.neo4j.cypher.internal.frontend.v2_3.ast.functions.Str;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -33,6 +32,8 @@ public class GetEnterpriseAndExpertData {
 //        getSQLExpertAndEnterpriseData();
         //得到机构和上级机构数据
         getInstitutionAndSuper();
+        //得到机构详细信息
+        getInstitutionDetailInfo();
     }
 
     /**
@@ -70,10 +71,40 @@ public class GetEnterpriseAndExpertData {
     }
 
 
+    /**
+     * 另外还有一个表存储 id，机构名，level，provinceid,province,city,cityid，
+     * institutionDetailInfoForImport
+     */
+    public void getInstitutionDetailInfo(){
+        System.out.println("start get InstitutionDetailInfo data from sql");
+        String sql ="select id,name,level,provinceid,province,city,cityid from EnterpriseInfo";
+        List<String> institutionDetailInfo = jdbcTemplate.query(sql, new RowMapper<String>(){
+            @Override
+            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+                String sql_id =  rs.getString("id");
+                String name =  rs.getString("name");
+                String level =  rs.getString("level");
+                String provinceid =  rs.getString("provinceid");
+                String province =  rs.getString("province");
+                String city =  rs.getString("city");
+                String cityid =  rs.getString("cityid");
+                if (name == null || "".equals(name))
+//                if (institutionName == null || "".equals(institutionName) ||
+//                        institutionName.contains("C") || institutionName.contains("T"))
+                    return "";
+//                System.out.println(uid+","+companyid+","+institutionName);
+                return (sql_id.trim()+","+name+","+level.trim()+","+provinceid.trim()+
+                        ","+province.trim()+","+city.trim()+","+cityid.trim()).replace("\n"," ");
+            }
+        });
+        System.out.println("institutionDetailInfo size: "+institutionDetailInfo);
+        writeInstitutionDetailInfo(institutionDetailInfo,basePath+File.separator+"py"+File.separator+"model","institutionDetailInfoForImport");
+    }
 
     /**
-     * 返回  论文uid对应的机构（论文uid，发表该论文的机构的id，发表该论文的机构的名字）
-     * HashMap<String,HashMap<String,String>> CSCD:5606807,46036,重庆声光电智联电子有限公司
+     * 返回  论文uid对应的机构（论文uid，发表该论文的机构的id，发表该论文的机构的名字）///////////////
+     * HashMap<String,HashMap<String,String>> CSCD:5606807,时间@@46036@@重庆声光电智联电子有限公司
+     * uid,论文时间，机构名，，，机构名，
      * @return
      */
 //    public HashMap<String,LinkedList<String>> getPaperInstitutionFormSql(){
@@ -81,11 +112,12 @@ public class GetEnterpriseAndExpertData {
         HashMap<String,HashMap<String,String>> tempInstitutionData = new HashMap<>();
         HashMap<String,LinkedList<String>> institutionData = new HashMap<>();
         System.out.println("start get paperInstution data from sql");
-        String sql ="select UID,companyid,organization from Address";
+        String sql ="select UID,pubyear,companyid,organization from Address";///////////////不一定有pubyear////////////////
         List<String> sqlDataList = jdbcTemplate.query(sql, new RowMapper<String>(){
             @Override
             public String mapRow(ResultSet rs, int rowNum) throws SQLException {
                 String uid =  rs.getString("UID");
+                String pubyear =  rs.getString("pubyear");
                 String companyid =  rs.getString("companyid");
                 String institutionName =  rs.getString("organization");
                 if (institutionName == null || "".equals(institutionName))
@@ -93,12 +125,12 @@ public class GetEnterpriseAndExpertData {
 //                        institutionName.contains("C") || institutionName.contains("T"))
                     return "";
 //                System.out.println(uid+","+companyid+","+institutionName);
-                return (uid.trim()+","+companyid.trim()+","+institutionName.trim()).replace("\n"," ");
+                return (uid.trim()+","+pubyear+"@@"+companyid.trim()+"@@"+institutionName.trim()).replace("\n"," ");
             }
         });
         for (String string : sqlDataList){
             if(string == null || "".equals(string)) continue;
-            String[] stringSplit = string.split(",");
+            String[] stringSplit = string.split("@@");
             if (stringSplit.length < 3) continue;
             if (tempInstitutionData.get(stringSplit[0]) == null){
                 HashMap<String,String> tempMap = new HashMap<>();
@@ -116,16 +148,16 @@ public class GetEnterpriseAndExpertData {
 
         //去除companyid
         int count = 0;
-        for (String uid : tempInstitutionData.keySet()){
+        for (String key : tempInstitutionData.keySet()){
 //            System.out.println(++count);
 //            if (institutionData.size() > 200000) break;
-            if (!institutionData.containsKey(uid)){
-                HashMap<String,String> tempMap = tempInstitutionData.get(uid);
+            if (!institutionData.containsKey(key)){
+                HashMap<String,String> tempMap = tempInstitutionData.get(key);
                 LinkedList<String> tempList = new LinkedList<>();
                 for (String companyid : tempMap.keySet()){
                     tempList.add(tempMap.get(companyid));
                 }
-                institutionData.put(uid,tempList);
+                institutionData.put(key,tempList);
             }
         }
 
@@ -150,8 +182,9 @@ public class GetEnterpriseAndExpertData {
     }
 
     /**
-     * 从数据库中得到撰写一片论文的共同作者
-     * HashMap<String,LinkedList<String>>  键为论文uid，值为作者列表
+     * 从数据库中得到撰写一片论文的共同作者 /////////////////////////////
+     * HashMap<String,LinkedList<String>>  键为论文uid,sql_id,时间，值为作者列表
+     * uid,sql_id,时间，作者，作者。。。，作者
      * @return
      */
 //    public HashMap<String,LinkedList<String>> getSQLExpertData(){
@@ -159,23 +192,25 @@ public class GetEnterpriseAndExpertData {
         HashMap<String,LinkedList<String>> expertData = new HashMap<>();
         System.out.println("start get expert data from sql");
 //        String sql ="select full_name from Author where UID in( select UID from paper)";
-        String sql ="select UID,full_name from Author";
+        String sql ="select UID,id,pubyear,full_name from Author";////////////////不一定有pubyear
         List<String> sqlDataList = jdbcTemplate.query(sql, new RowMapper<String>(){
             @Override
             public String mapRow(ResultSet rs, int rowNum) throws SQLException {
                 String uid =  rs.getString("UID");
                 String name =  rs.getString("full_name");
+                String sql_id =  rs.getString("id");
+                String pubyear =  rs.getString("pubyear");
 //                name = name.replaceAll("[a-zA-Z]","" );
                 if (name == null || "".equals(name))
                     return "";
 //                System.out.println(uid+","+name);
-                return uid.trim()+","+name.trim();
+                return uid.trim()+","+sql_id.trim()+","+pubyear.trim()+"@@"+name.trim();
             }
         });
 
         int count = 0;
         for (String string : sqlDataList){
-            String[] stringSplit = string.split(",");
+            String[] stringSplit = string.split("@@");
             if (stringSplit.length < 2) continue;
 //            System.out.println(++count);
 //            if (expertData.size() > 200000) break;
@@ -207,6 +242,35 @@ public class GetEnterpriseAndExpertData {
 //        return expertData;
     }
 
+//    public HashMap<String,String> getAddsqlIdExpertSQLData(String sql,String first, String second,String third){
+//        System.out.println("start get expert and enterprise data from sql");
+////        String sql ="select id,name from expert";
+////        String sql ="select id,name from enterpriseinfo";
+//        List<String> sqlDataList = jdbcTemplate.query(sql, new RowMapper<String>(){
+//            @Override
+//            public String mapRow(ResultSet rs, int rowNum) throws SQLException {
+//                String id =  rs.getString(first);
+////                String keywords =  rs.getString("keywords");
+//                String name =  rs.getString(second);
+//                if (id == null || name == null)
+//                    return "";
+////                System.out.println(id+","+name);
+//                return id.trim()+","+name.trim();
+//            }
+//        });
+//
+//        HashMap<String,String> map = new HashMap<>();
+//        int count = 0;
+//        for (String string : sqlDataList){
+//            if (string == null || "".equals(string)) continue;
+////            System.out.println(++count);
+////            if (map.size() > 200000) break;
+//            String[] stringSplit = string.split(",");
+//            map.put(stringSplit[0],stringSplit[1]);
+//        }
+////        System.out.println("data size: " + map.size());
+//        return map;
+//    }
 
     /**
      * 从数据空中得到属于某一企业的专家列表
@@ -219,11 +283,16 @@ public class GetEnterpriseAndExpertData {
         String sql_id_enterprise ="select id,name from enterpriseinfo";
         String sql_experid_enterpriseid ="select expertid,enterpriseid from expert2enterprise";
         //专家id为键，name为值
-        HashMap<String,String> id2expert = getSQLData(sql_id_expert,"id","name");
+        HashMap<String,String> id2expert = getSQLData(sql_id_expert,"id","name");////这个id就是数据库id
+        //name为键，专家id为值
+        HashMap<String,String> expert2id = new HashMap<>();
+        for (String id : id2expert.keySet()){
+            expert2id.put(id2expert.get(id),id);
+        }
         //企业id为键，name为值
         HashMap<String,String> id2enterprise = getSQLData(sql_id_enterprise,"id","name");
         //专家id为键，企业id为值
-        HashMap<String,String> experid2enterpriseid = getSQLData(sql_experid_enterpriseid,"expertid","enterpriseid");
+        HashMap<String,String> experid2enterpriseid = getSQLData(sql_experid_enterpriseid,"expertid","enterpriseid");//////
 
         HashMap<String,LinkedList<String>> enterpriseAndExpert = new HashMap<>();
         for (String expertid : experid2enterpriseid.keySet()){
@@ -271,7 +340,8 @@ public class GetEnterpriseAndExpertData {
 //        writeEnterpriseAndExpert(enterpriseAndExpert,"E:\\tech_analysis_my\\tech_analysis\\py\\model","enterpriseAndExpertForImport");
 //        writeName2Enterprise(name2Enterprise,"E:\\tech_analysis_my\\tech_analysis\\py\\model","name2EnterpriseForImport");
         writeEnterpriseAndExpert(enterpriseAndExpert,basePath+File.separator+"py"+File.separator+"model","enterpriseAndExpertForImport");
-        writeName2Enterprise(name2Enterprise,basePath+File.separator+"py"+File.separator+"model","name2EnterpriseForImport");
+        //author_sql_id,专家,机构
+        writeName2Enterprise(name2Enterprise,expert2id,basePath+File.separator+"py"+File.separator+"model","name2EnterpriseForImport");
         enterpriseAndExpert.clear();
         name2Enterprise.clear();
 //        return enterpriseAndExpert;
@@ -374,13 +444,14 @@ public class GetEnterpriseAndExpertData {
 
     /**
      * 写入专家和所属的企业
-     * 第一列为专家名  第二列为所属企业名
+     *
+     * author_sql_id,专家,机构////////////////////////
      * @param name2Enterprise
      * @param filePath
      * @param fileName
      * @return
      */
-    public boolean writeName2Enterprise(HashMap<String,String> name2Enterprise,String filePath, String fileName){
+    public boolean writeName2Enterprise(HashMap<String,String> name2Enterprise,HashMap<String,String> expert2id,String filePath, String fileName){
         // 标记文件生成是否成功
         boolean flag = true;
 
@@ -406,7 +477,7 @@ public class GetEnterpriseAndExpertData {
             // 将格式化后的字符串写入文件
             Writer write = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
             for (String expertName : name2Enterprise.keySet()){
-                String temp = expertName+","+name2Enterprise.get(expertName);
+                String temp = expert2id.get(expertName)+","+expertName+","+name2Enterprise.get(expertName);
                 write.write(temp);
                 write.write('\n');
                 ++countline;
@@ -425,7 +496,7 @@ public class GetEnterpriseAndExpertData {
     }
 
     /**
-     *写入论文uid和论文合著作者
+     *写入论文uid和论文合著作者//////////////////////////////////
      * 第一列为uid  其后为合著该论文的作者
      * @param expertData  键为论文uid，值为作者列表
      * @param filePath
@@ -457,9 +528,9 @@ public class GetEnterpriseAndExpertData {
 
             // 将格式化后的字符串写入文件
             Writer write = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
-            for (String uid : expertData.keySet()){
-                String temp = uid;
-                for (String expertName : expertData.get(uid)){
+            for (String key : expertData.keySet()){
+                String temp = key;
+                for (String expertName : expertData.get(key)){
                     temp += ","+expertName;
                 }
                 write.write(temp);
@@ -468,10 +539,10 @@ public class GetEnterpriseAndExpertData {
             }
             write.flush();
             write.close();
-            System.out.println("论文uid和论文作者CSV写入成功！");
+            System.out.println("论文uid,id,时间和论文作者CSV写入成功！");
         } catch (IOException e) {
             flag = false;
-            System.out.println("论文uid和论文作者CSV写入失败 !");
+            System.out.println("论文uid,id,时间论文作者CSV写入失败 !");
             e.printStackTrace();
         }
         System.out.println(countline);
@@ -579,4 +650,49 @@ public class GetEnterpriseAndExpertData {
         return flag;
 
     }
+
+    boolean writeInstitutionDetailInfo(List<String> institutionDetailInfo,String filePath, String fileName){
+        // 标记文件生成是否成功
+        boolean flag = true;
+
+        //文件总行数
+        long countline = 0;
+
+        // 拼接文件完整路径
+        String fullPath = filePath + File.separator + fileName + ".csv";
+
+        // 生成csv格式文件
+        try {
+            // 保证创建一个新文件
+            File file = new File(fullPath);
+            if (!file.getParentFile().exists()) { // 如果父目录不存在，创建父目录
+                file.getParentFile().mkdirs();
+            }
+            if (file.exists()) { // 如果已存在,删除旧文件
+                file.delete();
+            }
+            file.createNewFile();
+
+
+            // 将格式化后的字符串写入文件
+            Writer write = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+            for (String info : institutionDetailInfo){
+                if (info == null || "".equals(info)) continue;
+                write.write(info);
+                write.write('\n');
+                ++countline;
+            }
+            write.flush();
+            write.close();
+            System.out.println("机构详细信息CSV写入成功！");
+        } catch (IOException e) {
+            flag = false;
+            System.out.println("机构详细信息CSV写入失败 !");
+            e.printStackTrace();
+        }
+        System.out.println(countline);
+        // 返回是否成功的标记
+        return flag;
+    }
+
 }
